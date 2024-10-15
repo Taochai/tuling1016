f00ad205ad4fb0f6293fd0dd45d93599a1599d80
taochai <12371486+taochai123@user.noreply.gitee.com>
2024-10-16 02:22:45 +0800

    1016

--- tulingmall-unqid/src/main/java/com/tuling/tulingmall/config/SystemConf.java
+++ tulingmall-unqid/src/main/java/com/tuling/tulingmall/config/SystemConf.java
@@ -2,7 +2,6 @@ package com.tuling.tulingmall.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
